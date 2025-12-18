bc140a99f891273ef220b42ee4e15f7639b56766
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2025-12-19 01:10:56 +0530

    [Bhumika Shrivas]: Add FactorsOfNumberUsingWhile.java

---
+++ gcr-codebase/control-flow/Level-2-Programs/FactorsOfNumberUsingWhile.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FactorsOfNumberUsingWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number <= 0) 
+		{
+            System.out.println("Please enter a positive integer");
+        } 
+		else 
+		{
+            System.out.println("Factors of " + number + " are:");
+            int i = 1;
+            while (i < number) 
+			{
+                if (number % i == 0) 
+				{
+                    System.out.println(i);
+                }
+                i++;
+            }
+        }
+        input.close();
+    }
+}
