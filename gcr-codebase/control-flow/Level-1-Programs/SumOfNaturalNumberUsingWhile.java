778c9b4efa2ad5e788a0d0c28ec3fdc2dc88b047
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2025-12-18 23:43:26 +0530

    [Bhumika Shrivas]: Add SumOfNaturalNumberUsingWhile.java

---
+++ gcr-codebase/control-flow/Level-1-Programs/SumOfNaturalNumberUsingWhile.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumberUsingWhile
+{
+	public static void main(String [] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		//Take the user input number
+		System.out.print("Enter a natural number: ");
+		int n=input.nextInt();
+		
+		if (n <= 0) 
+		{
+            System.out.println("The number is not a natural number");
+        } 
+		else 
+		{
+            // Use while loop
+            int sumByLoop = 0;
+            int i = 1;
+            while (i <= n) 
+			{
+                sumByLoop += i;
+                i++;
+            }
+
+            // Use formula
+            int sumByFormula = n * (n + 1) / 2;
+
+            // Compare results
+            System.out.println("Sum using while loop = " + sumByLoop);
+            System.out.println("Sum using formula = " + sumByFormula);
+
+            if (sumByLoop == sumByFormula) 
+			{
+                System.out.println("Both results are correct and equal");
+            } 
+			else 
+			{
+                System.out.println("Results are not equal");
+            }
+        }
+
+        input.close();
+    }
+}
