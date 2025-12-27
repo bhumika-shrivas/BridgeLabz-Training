37ef2d3dbd2210edc331639034a674ca77a62f73
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2025-12-27 23:57:06 +0530

    [Bhumika Shrivas]: Added Book.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/Book.java
@@ -0,0 +1,24 @@
+package com.constructor.level1;
+
+public class Book {
+
+	String title;
+	String author;
+	double price;
+	
+	public Book () {
+		
+	}
+	
+	public Book(String title, String author , double price){
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+	
+	public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}	
