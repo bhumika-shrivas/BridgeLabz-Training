1f12a51fc943b2f23383978aaa913df269a797bd
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2025-12-28 00:02:02 +0530

    [Bhumika Shrivas]: Added Person.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/Person.java
@@ -0,0 +1,24 @@
+package com.constructor.level1;
+
+public class Person {
+	
+	String name;
+    int age;
+
+    // Parameterized constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    public Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+	
+}
