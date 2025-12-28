b83f60ea283f8d0aaf8540ed7bb3419d275799b2
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2025-12-28 15:05:47 +0530

    [Bhumika Shrivas]: Added BankAccount.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/BankAccount.java
@@ -0,0 +1,45 @@
+package com.constructor.level1;
+
+public class BankAccount {
+
+    public long accountNumber;
+    protected String accountHolder;
+    private double balance;
+    
+    public BankAccount(long accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    // Getter for balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Setter for balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // Deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    // Withdraw method
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    // Display account details
+    public void displayAccountDetails() {
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolder);
+        System.out.println("Balance        : ₹" + balance);
+    }
+}
