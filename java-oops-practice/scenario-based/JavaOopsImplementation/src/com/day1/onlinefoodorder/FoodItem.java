1ab14980c016678d3377ebc2b4dc2c5fc6f1d658
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 15:42:40 +0530

    [Bhumika Shrivas]: Added FoodItem

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day1/onlinefoodorder/FoodItem.java
@@ -0,0 +1,12 @@
+package com.day1.onlinefoodorder;
+
+public class FoodItem 
+{
+    String name;
+    double price;
+
+    public FoodItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
