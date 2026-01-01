18050fc7aec0febe94d633a0a8017c8bef6d965d
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:22:25 +0530

    [Bhumika Shrivas]: Added Order.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/mutilevelinheritance/ordermanagement/Order.java
@@ -0,0 +1,17 @@
+package com.inheritance.mutilevelinheritance.ordermanagement;
+
+// Base class
+public class Order {
+
+    protected int orderId;
+    protected String orderDate;
+
+    public Order(int orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return "Order Placed";
+    }
+}
