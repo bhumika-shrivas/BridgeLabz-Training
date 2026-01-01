b5d7d1a14bb9b8987991de9dcf12f1e2dff3b91c
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 15:33:09 +0530

    [Bhumika Shrivas]: Added CurrentAccount.java

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day1/banksystem/CurrentAccount.java
@@ -0,0 +1,9 @@
+package com.day1.banksystem;
+
+// Subclass
+public class CurrentAccount extends Account {
+
+    public CurrentAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+}
