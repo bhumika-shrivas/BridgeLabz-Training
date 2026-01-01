2cad0289a1c38e3908adbd90b26b2d9bd57422fc
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:39:26 +0530

    [Bhumika Shrivas]: Added Vehicle.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagement/Vehicle.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+// Superclass
+public class Vehicle {
+
+    protected String model;
+    protected int maxSpeed;
+
+    public Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void displayInfo() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
