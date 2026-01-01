9534c3017fc35eb18a6c79c019c2f0dc5478fbf1
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:22:05 +0530

    [Bhumika Shrivas]: Added DeliverOrder.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/mutilevelinheritance/ordermanagement/DeliveredOrder.java
@@ -0,0 +1,18 @@
+package com.inheritance.mutilevelinheritance.ordermanagement;
+
+// Subclass (extends ShippedOrder)
+public class DeliveredOrder extends ShippedOrder {
+
+    private String deliveryDate;
+
+    public DeliveredOrder(int orderId, String orderDate,
+                           String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    public String getOrderStatus() {
+        return "Order Delivered on " + deliveryDate;
+    }
+}
