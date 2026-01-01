caf8d4703100f20ef2a747cbc0ab2496655a666a
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:23:26 +0530

    [Bhumika Shrivas]: Added ShippedOrder.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/mutilevelinheritance/ordermanagement/ShippedOrder.java
@@ -0,0 +1,17 @@
+package com.inheritance.mutilevelinheritance.ordermanagement;
+
+// subclass
+public class ShippedOrder extends Order {
+
+    protected String trackingNumber;
+
+    public ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    public String getOrderStatus() {
+        return "Order Shipped (Tracking No: " + trackingNumber + ")";
+    }
+}
