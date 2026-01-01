37362c979088dfe622f9558327a37d4ea43cc339
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 15:47:26 +0530

    [Bhumika Shrivas]: Added Order.java

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day1/onlinefoodorder/Order.java
@@ -0,0 +1,9 @@
+package com.day1.onlinefoodorder;
+
+public class Order 
+{
+    public void placeOrder(double amount) 
+    {
+        System.out.println("Order placed. Total bill: ₹" + amount);
+    }
+}
