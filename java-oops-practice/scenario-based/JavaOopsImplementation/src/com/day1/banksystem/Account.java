84e4ed1477d5a04c18faeb83ba8eab3149adfac4
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 15:31:04 +0530

    [Bhumika Shrivas]: Added Account.java

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day1/banksystem/Account.java
@@ -0,0 +1,33 @@
+package com.day1.banksystem;
+
+// Base Class
+public class Account {
+
+    protected String accountNumber;
+    protected double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited ₹" + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn ₹" + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Current Balance: ₹" + balance);
+    }
+}
