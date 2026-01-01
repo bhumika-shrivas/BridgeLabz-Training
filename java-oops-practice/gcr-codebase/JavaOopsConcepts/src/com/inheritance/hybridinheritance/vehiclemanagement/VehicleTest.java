2e52c668d2059bea3da197f87390b495f3df4fb7
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:40:07 +0530

    [Bhumika Shrivas]: Added VehicleTest.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagement/VehicleTest.java
@@ -0,0 +1,18 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+// Main class
+public class VehicleTest {
+    public static void main(String[] args) {
+
+        ElectricVehicle ev = new ElectricVehicle("Tesla Model 3", 200);
+        PetrolVehicle pv = new PetrolVehicle("Honda City", 180);
+
+        ev.displayInfo();
+        ev.charge();
+
+        System.out.println();
+
+        pv.displayInfo();
+        pv.refuel();
+    }
+}
