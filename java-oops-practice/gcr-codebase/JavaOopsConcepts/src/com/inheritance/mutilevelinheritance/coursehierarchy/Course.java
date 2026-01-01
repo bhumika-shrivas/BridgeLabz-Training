8ab299efa01d1139c83b40680d290fd00111c8b4
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:16:13 +0530

    [Bhumika Shrivas]: Added Course.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/mutilevelinheritance/coursehierarchy/Course.java
@@ -0,0 +1,18 @@
+package com.inheritance.mutilevelinheritance.coursehierarchy;
+
+// Base Class
+public class Course {
+
+    protected String courseName;
+    protected int duration; // in hours
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public void displayDetails() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " hours");
+    }
+}
