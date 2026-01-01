9a319803302545a44db6015ecdb444ed53bfacba
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-01 12:49:49 +0530

    [Bhumika Shrivas]: Added Intern.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employeemanagementsystem/Intern.java
@@ -0,0 +1,18 @@
+package com.inheritance.assistedproblems.employeemanagementsystem;
+
+// Subclass
+public class Intern extends Employee {
+
+    private int durationMonths;
+
+    public Intern(String name, int id, double salary, int durationMonths) {
+        super(name, id, salary);
+        this.durationMonths = durationMonths;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + durationMonths + " months");
+    }
+}
