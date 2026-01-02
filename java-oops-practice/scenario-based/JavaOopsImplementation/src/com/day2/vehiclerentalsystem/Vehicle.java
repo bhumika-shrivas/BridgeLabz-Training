90c2b9b4aede42457cbdc846a2394cbc1a7c34f2
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-02 16:30:46 +0530

    [Bhumika Shrivas]: Add Vehicle program file

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day2/vehiclerentalsystem/Vehicle.java
@@ -0,0 +1,20 @@
+package com.day2.vehiclerentalsystem;
+
+// Base Class – Vehicle (Encapsulation + Inheritance)
+abstract class Vehicle implements Rentable {
+    protected String vehicleNumber;
+    protected double baseRate;
+
+    Vehicle(String vehicleNumber, double baseRate) {
+        this.vehicleNumber = vehicleNumber;
+        this.baseRate = baseRate;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public double getBaseRate() {
+        return baseRate;
+    }
+}
