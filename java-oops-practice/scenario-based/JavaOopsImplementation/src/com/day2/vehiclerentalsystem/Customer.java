0f3a089fb0abff2f31739f282aedb3835c598787
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-02 16:26:13 +0530

    [Bhumika Shrivas]: Add Customer program file

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day2/vehiclerentalsystem/Customer.java
@@ -0,0 +1,16 @@
+package com.day2.vehiclerentalsystem;
+
+class Customer 
+{
+    private String name;
+
+    Customer(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+}
