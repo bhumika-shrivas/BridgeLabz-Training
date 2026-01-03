e288dde5fdd9a5ddee810d5d09494d869b0989c0
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-03 13:31:17 +0530

    [Bhumika Shrivas]: Add Driver program file

---
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day3/cabbygo/Driver.java
@@ -0,0 +1,17 @@
+package com.day3.cabbygo;
+
+class Driver {
+    private String name;
+    private String licenseNumber;
+    private double rating; // private
+
+    Driver(String name, String licenseNumber, double rating) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.rating = rating;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
