bf894a9d378d646b2652bb86d9da4400b7a3b6f9
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-15 11:55:13 +0530

    [Bhumika Shrivas]: Update code logic

--- java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day10/birdsanctuary/Bird.java
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day10/birdsanctuary/Bird.java
@@ -2,14 +2,27 @@ package com.day10.birdsanctuary;
 
 // Base class
 abstract class Bird {
-    String name;
-    String species;
-    int id;
+
+    private int id;
+    private String name;
+    private String species;
 
     Bird(int id, String name, String species) {
         this.id = id;
         this.name = name;
         this.species = species;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
     }
 
     void eat() {
