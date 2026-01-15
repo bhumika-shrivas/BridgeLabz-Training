bfe8e91d151a3caec50241f900abed536a6f33b7
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-15 12:14:41 +0530

    [Bhumika Shrivas]: Update code logic

--- java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day10/birdsanctuary/Eagle.java
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day10/birdsanctuary/Eagle.java
@@ -2,10 +2,12 @@ package com.day10.birdsanctuary;
 
 // Derived Class
 public class Eagle extends Bird implements Flyable {
+
     Eagle(int id, String name) {
         super(id, name, "Eagle");
     }
+
     public void fly() {
-        System.out.println(name + " soars high");
+        System.out.println(getName() + " is flying high");
     }
 }
--- java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day10/birdsanctuary/Penguin.java
+++ java-oops-practice/scenario-based/JavaOopsImplementation/src/com/day10/birdsanctuary/Penguin.java
@@ -2,10 +2,12 @@ package com.day10.birdsanctuary;
 
 // Derived class
 public class Penguin extends Bird implements Swimmable {
+
     Penguin(int id, String name) {
         super(id, name, "Penguin");
     }
+
     public void swim() {
-        System.out.println(name + " swims expertly");
+        System.out.println(getName() + " is swimming");
     }
 }
