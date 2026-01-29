c7c4ce72f618fbdac504b81486a51989460548a0
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-01-29 23:39:16 +0530

    [Bhumika Shrivas]: Add reflection basiclevel program files

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/basiclevel/dynamicallycreateobjects/Student.java
@@ -0,0 +1,13 @@
+package com.reflection.basiclevel.dynamicallycreateobjects;
+
+public class Student {
+
+	public Student() {
+        System.out.println("Student object created");
+    }
+
+    public void display() {
+        System.out.println("Hello student");
+    }
+}
+
