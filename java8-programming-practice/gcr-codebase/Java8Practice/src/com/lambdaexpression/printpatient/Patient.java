322a3fba738c1af2f69ddc5317934896c02c8b60
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-02-02 01:39:42 +0530

    [Bhumika Shrivas]: Add printpatient program files

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/lambdaexpression/printpatient/Patient.java
@@ -0,0 +1,16 @@
+package com.lambdaexpression.printpatient;
+
+public class Patient {
+
+    private int id;
+    private String name;
+
+    public Patient(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
