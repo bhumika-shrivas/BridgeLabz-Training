4e4d71dd10df7a2e63e51f41e6841f345dc1660b
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2026-02-09 09:19:11 +0530

    [Bhumika Shrivas]: Add Bank Transaction Multithreading System

---
+++ java-design-pattern-practice/scenario-based/ScenarioProblems/src/com/banktransactionmulithreading/Account.java
@@ -0,0 +1,27 @@
+package com.banktransactionmulithreading;
+
+public class Account {
+	private int balance;
+
+	public Account(int balance) {
+		this.balance = balance;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void deposit(int amount) {
+		balance += amount;
+	}
+
+	public boolean withdraw(int amount) {
+		if (balance >= amount) {
+			balance -= amount;
+			return true;
+		}
+		return false;
+	}
+}
+
+
